2d4b95ef0ad282e4a615c462281d460623a56a8d
Manoharan Gurusamy <manoharan.gurusamy@gmail.com>
2019-11-05 12:25:30 +0000

    added comments to newFeature.js file

--- newFeature.js
+++ newFeature.js
@@ -1,3 +1,4 @@
+// This is going to contain new feature as requested by business
 function newFeature(){
     console.log("new feature has been developed by group of our team members");
-}This is going to contain new feature as requested by business
+}
